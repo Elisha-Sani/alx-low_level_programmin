c29df9c6480d4cf83b788edd67830c8b45c27b4a
Elisha-Sani <elishamuuo1@gmail.com>
2023-11-24 13:38:35 -0800

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,11 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-
+#ifdef __x86_64__
+    printf("Running on 64-bit architecture\n");
+  #else
+    printf("Running on 32-bit architecture\n");
+  #endif
 printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(a));
 printf("Size of a int: %lu bytes(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
