17b17299cfbe8c851f12d38fdc16a0bbb1cd47d2
Elisha-Sani <elishamuuo1@gmail.com>
2023-11-25 10:49:59 -0800

    P and N

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,21 +2,21 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Function that confirms whether number is positive or negative
+ * main - Function that checks sign
  * Return: 0 (Success)
-*/
-int  main(void)
+ */
+int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	if (n = 0)
+	if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
