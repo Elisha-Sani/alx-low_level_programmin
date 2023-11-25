239074aefb77c6f4b893aa1a99e4d3a4dfec0fb8
Elisha-Sani <elishamuuo1@gmail.com>
2023-11-24 17:17:29 -0800

    binary rep

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,36 +10,10 @@ void print_binary(unsigned long int n)
 {
 	int i = 31;
 
-	while (i >= 0)
+	if (n >= 2)
 	{
-	if (n & (1 << i))
-	{
-		printf("1");
-	}
-	else
-	{
-		print("0");
-	}
-	i--;
+		print_binary(n >> 1);
+
+	putchar((n & 1) + '0');
 	}
 }
-
-/**
- * main - defines the entry point of the program
- * print_binary - defines each individual numbe
- * Return: 0 (successr)
- */
-
-int main(void)
-{
-print_binary(0);
-printf("\n");
-print_binary(1);
-printf("\n");
-print_binary(98);
-printf("\n");
-print_binary(1024);
-printf("\n");
-print_binary((1 << 10) + 1);
-printf("\n");
-return (0);
