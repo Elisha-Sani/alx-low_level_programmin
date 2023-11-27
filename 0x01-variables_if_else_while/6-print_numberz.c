7a06228865cd84d4bff65f7ed1951b5481a1339d
Elisha-Sani <elishamuuo1@gmail.com>
2023-11-27 08:56:06 -0800

    Numbers without char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * main - Function that prints out single digit numbers without using char
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
