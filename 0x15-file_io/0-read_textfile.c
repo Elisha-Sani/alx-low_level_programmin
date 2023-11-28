02893a7e730a6c9859ceb1ae6e39222934cf8d19
Elisha-Sani <elishamuuo1@gmail.com>
2023-11-27 19:56:33 -0800

    read and output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcnt1.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to the stdo
+ *
+ * @filename: name of the file to read
+ *
+ * @letters: number of letters it should read and output
+ *
+ * Return: The actual number of letters it should read and output
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_d;
+	ssize_t lenr, lenw;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	file_n = open(filename, O_RDONLY);
+	if (file_d == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(file_d);
+		return (0);
+	}
+	lenr = read(file_d, buffer, letters);
+	close(file_d);
+	if (lenr == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
+}
