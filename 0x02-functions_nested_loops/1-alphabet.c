1045e47197709328a1c10b4e0aa4e92cc0e1b19d
Elisha-Sani <elishamuuo1@gmail.com>
2023-12-12 10:30:19 -0800

    Alphabet in lower

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_alphabet - Prints alphabets in lowercase
+ */
+void print_alphabet(void)
+{
+	int l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		_putchar('l');
+	}
+
+	_putchar('\n');
+}
